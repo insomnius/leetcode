99aecbcbbd9baf952d9b7fe85fe2043bb1256317
Insomnius <aliana.arib@gmail.com>
2022-12-31 17:15:33 +0700

    Add really hacky solutions

--- golang/group_anagram.go
+++ golang/group_anagram.go
@@ -1,5 +1,33 @@
 package leetcode
 
+import "fmt"
+
+// 49. Group Anagrams
+
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+// Constraints:
+
+// 1 <= strs.length <= 104
+// 0 <= strs[i].length <= 100
+// strs[i] consists of lowercase English letters.
+
+// This solution actually got time limited, and it's not the best solution.
 func groupAnagrams(strs []string) [][]string {
 	anagrams := [][]string{}
 	if len(strs) == 1 {
@@ -21,6 +49,36 @@ func groupAnagrams(strs []string) [][]string {
 		}
 
 		anagrams = append(anagrams, []string{str})
+	}
+
+	return anagrams
+}
+
+func groupAnagramsAlt(strs []string) [][]string {
+	anagrams := [][]string{}
+	if len(strs) == 1 {
+		return append(anagrams, strs)
+	}
+
+	anagramHashMap := map[string][]string{}
+
+	for _, str := range strs {
+		arr := make([]int, 26)
+		for _, char := range str {
+			arr[char-'a']++
+		}
+
+		arrToS := fmt.Sprintf("%v", arr)
+		anagramHashMap[arrToS] = append(anagramHashMap[arrToS], str)
+	}
+
+	for _, hashMap := range anagramHashMap {
+		anagramArr := []string{}
+
+		for _, v := range hashMap {
+			anagramArr = append(anagramArr, v)
+		}
+		anagrams = append(anagrams, anagramArr)
 	}
 
 	return anagrams
--- golang/group_anagram_test.go
+++ golang/group_anagram_test.go
@@ -1,11 +1,36 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGroupAnagram(t *testing.T) {
 
 	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
 	result := groupAnagrams(strs)
+
+	if len(result) != 3 {
+		t.Errorf("Expected 3, got %d", len(result))
+	}
+
+	if len(result[0]) != 3 {
+		t.Errorf("Expected 3, got %d", len(result[0]))
+	}
+
+	if len(result[1]) != 2 {
+		t.Errorf("Expected 2, got %d", len(result[1]))
+	}
+
+	if len(result[2]) != 1 {
+		t.Errorf("Expected 1, got %d", len(result[2]))
+	}
+
+}
+
+func TestGroupAnagramAlt(t *testing.T) {
+
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	result := groupAnagramsAlt(strs)
 
 	if len(result) != 3 {
 		t.Errorf("Expected 3, got %d", len(result))
